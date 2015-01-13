78108e3e662f6b30315476a673d44f09f31499e2
Aditi Joshi <aditisatishjoshi@gmail.com>
2015-01-12 21:58:18 -0500

    added vex files

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	
+	$("#insper").click(function() {
+		vex.dialog.alert('Thanks for checking out Vex!');
+	});
+	
+	$("#research").click(function() {
+		vex.dialog.alert('Thanks for checking out Vex!');
+	});
+
+	$("#e4h").click(function() {
+		vex.dialog.alert('Thanks for checking out Vex!');
+	});
+
+	$("#modcon").click(function() {
+		vex.dialog.alert('Thanks for checking out Vex!');
+	});
+
+	$("#edisco").click(function() {
+		vex.dialog.alert('Thanks for checking out Vex!');
+	});
+
+	$("#rwn").click(function() {
+		vex.dialog.alert('Thanks for checking out Vex!');
+	});
+
+	$("#documentary").click(function() {
+		vex.dialog.alert('Thanks for checking out Vex!');
+	});
+
+	$("#desnat").click(function() {
+		vex.dialog.alert('Thanks for checking out Vex!');
+	});
+});
